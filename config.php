7b72e76fb2ba48a4c8f53f05af74a0a2db8093f9
bhaskar-kj <66899804+bhaskar-kj@users.noreply.github.com>
2021-01-18 23:11:19 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+		$conn = mysqli_connect('localhost','id15658857_akansha_vatsa','Akanshavatsa@123','id15658857_foundation_bank');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
