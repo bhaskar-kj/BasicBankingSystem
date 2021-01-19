0b0f9b65754773e1887a1f2248a81de9fcd53cea
bhaskar-kj <66899804+bhaskar-kj@users.noreply.github.com>
2021-01-19 12:14:41 +0530

    Update transactionhistory.php

--- transactionhistory.php
+++ transactionhistory.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Transaction History</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/table.css">
